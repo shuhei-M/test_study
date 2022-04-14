0097b1fcaa4ab8cf3879b2bba83a434226f16bb5
imagire <imagire@gmail.com>
2022-04-14 20:38:36 +0900

    add files

---
+++ src/StaticLib/StaticLib.cpp
@@ -0,0 +1,12 @@
+﻿// StaticLib.cpp : スタティック ライブラリ用の関数を定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../include/StaticLib.h"
+
+
+bool is_leap_year(int year)
+{
+	return true;
+}
---
+++ src/include/StaticLib.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// ‚¤‚é‚¤”N‚Ì”»’è
+bool is_leap_year(int year);
